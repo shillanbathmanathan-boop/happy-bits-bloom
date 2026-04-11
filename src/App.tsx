a5f70833e61cb47b813df15ab6fbd9e2f30ebc95
shillanbathmanathan-boop <shillanbathmanathan@gmail.com>
2026-04-12 07:24:50 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index";
-import Pilots from "./pages/Pilots";
+// FIXED: Using curly braces because Pilots is a named export in Pilots.tsx
+import { Pilots } from "./pages/Pilots"; 
 import PilotProfile from "./pages/PilotProfile";
 import Register from "./pages/Register";
 import Terms from "./pages/Terms";
